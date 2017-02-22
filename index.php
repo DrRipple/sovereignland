5c2a2b441ab724776a82a0ce8d302bf73de1ede0
Cooper Johnston <sverige.cooper@gmail.com>
2017-02-22 08:33:13 -0800

    rearranged files, added sftp plugin

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Sovereign.Land</title>
-    </head>
-    <body>
-        <h1>Hello, world.</h1>
-    </body>
+	<head>
+		<title>Sovereign.Land</title>
+		<link rel="icon" type="image/png" href="favicon.png">
+	</head>
+	<body>
+		<h1>Hello, world!</h1>
+		<p>Sovereign.land is currently in development.</p>
+		<p>Nation simulator game by Solborg.</p>
+	</body>
 </html>
