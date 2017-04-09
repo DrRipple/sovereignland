7c34168545230b145431a78e433e2dc9122c1a03
Cooper <sverige.cooper@gmail.com>
2017-04-08 22:37:40 -0700

    added non-functional phase 2 of nation making

--- html/new/nation2.php
+++ html/new/nation2.php
@@ -42,10 +42,12 @@ if (isset($_POST["token"])) {
     $dirpath = "../data/nations/$world/" . $dirname;
     mkdir($dirpath, 0755, true);
 
+    $officialtitle = test_input($_POST["official"]);
+
     $basics_data = array(
         "userID" => $userID,
         "name" => test_input($_POST["name"]),
-        "official" => test_input($_POST["official"]),
+        "official" => $officialtitle,
         "ccode" => test_input($_POST["currency"]),
         "cvalue" => test_input($_POST["cvalue"]),
         "flagURL" => test_input($_POST["flag"])
@@ -55,8 +57,8 @@ if (isset($_POST["token"])) {
     fwrite($basicsfile, $basics);
     fclose($basicsfile);
 } else {
-    header("Location: http://sovereign.land/newnation/");
-    die();
+    $nation = "Test!";
+    $officialtitle = "The Repubic of Test";
 }
 ?>
 <html>
@@ -66,6 +68,11 @@ if (isset($_POST["token"])) {
 		<link rel="stylesheet" href="style.css">
         
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:700|Roboto:400,400i,700,700i">
+
+        <meta name="google-signin-client_id" content="1057622173913-bpi238tov8so32pbm4lj12u4elordq20.apps.googleusercontent.com">
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     </head>
     <body>
     	<div id="topbar">
@@ -76,13 +83,165 @@ if (isset($_POST["token"])) {
                 </div>
             </div>
         </div>
-    	<form action="phase2.php" method="post">
+    	<form action="nation3.php" method="post">
+            <span class="textfield">
+                <p>Land Area (km<sup>2</sup>)</p>
+                <input type="number" name="civ_area">
+                <p class="helptext">Enter the total land area of your country.</p>
+            </span>
+            <span class="textfield">
+                <p>Population Density (people/km<sup>2</sup>)</p>
+                <input type="number" name="civ_density">
+                <p class="helptext">Enter the overall population density of your country.</p>
+            </span>
+
+            <hr>
+
     		<span class="textfield">
-	    		<p>Nation Name</p>
-	    		<input type="text" name="name">
-	    		<p class="helptext">Enter the common name of your nation (ex. Finland).</p>
+	    		<p>Economic System</p>
+	    		<select name="econ_sys" onchange="updateEconFields()" id="econSys">
+                    <option value="cap">Capitalist</option>
+                    <option value="soc">Socialist</option>
+                </select>
+	    		<p class="helptext">Choose the basis of your nation's economic system.</p>
 	    	</span>
-	    	<button>Proceed to Next Step</button>
+            <span class="textfield">
+                <p>Personal Income Tax (%)</p>
+                <input type="number" name="econ_ptax">
+                <p class="helptext">Enter the personal income tax rate of an average worker in your country.</p>
+            </span>
+            <span class="textfield" id="corpTax">
+                <p>Corporate Income Tax (%)</p>
+                <input type="number" name="econ_ctax">
+                <p class="helptext">Enter the corporate income tax rate for a medium sized business in your country.</p>
+            </span>
+
+            <hr>
+
+            <div id="piechart"></div>
+            <div id="budget">
+                <p><b>Budget Points</b></p>
+                <p style="font-size: 14px; color: #777">Adjust the points to change the percent budget allocated to each department. Only the percentages will be recorded.</p>
+
+                <p>Defense</p>
+                <input type="number" name="bud_def" value="10" onchange="updateBudgetChart()" class="budgetValue">
+
+                <p>Healthcare</p>
+                <input type="number" name="bud_health" value="10" onchange="updateBudgetChart()" class="budgetValue">
+
+                <p>Education</p>
+                <input type="number" name="bud_edu" value="10" onchange="updateBudgetChart()" class="budgetValue">
+
+                <p>Energy</p>
+                <input type="number" name="bud_ene" value="10" onchange="updateBudgetChart()" class="budgetValue">
+
+                <p>Science</p>
+                <input type="number" name="bud_sci" value="10" onchange="updateBudgetChart()" class="budgetValue">
+
+                <p>Transportation</p>
+                <input type="number" name="bud_trans" value="10" onchange="updateBudgetChart()" class="budgetValue">
+
+                <p>Business</p>
+                <input type="number" name="bud_bus" value="10" onchange="updateBudgetChart()" class="budgetValue">
+
+                <p>Administration</p>
+                <input type="number" name="bud_admin" value="10" onchange="updateBudgetChart()" class="budgetValue">
+            </div>
+
+            <hr>
+
+            <div>
+                <p><b>Wiki Entry</b></p>
+                <p>Formatted using <a href="https://guides.github.com/features/mastering-markdown/" target="_blank">Markdown</a>.</p>
+                <textarea id="wiki"># <?php echo $officialtitle ?></textarea>
+            </div>
+	    	
+            <hr>
+
+            <span class="textfield">
+                <p id="g_signin_text">Sign-In With Google</p>
+                <div class="g-signin2" data-onsuccess="onSignIn"></div>
+            </span>
+
+            <span class="textfield" id="finalbutton">
+                <p>By clicking the button below, you confirm that you have read and will abide by the <a href="#" target="_blank">Terms and Conditions</a>.</p>
+                <button>Proceed to Next Step</button>
+            </span>
     	</form>
+
+        <script>
+            google.charts.load("current", {"packages": ["corechart"]});
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+                var data = google.visualization.arrayToDataTable([
+                    ["Department", "% Allocated"],
+                    ["Defense", 10],
+                    ["Healthcare", 10],
+                    ["Education", 10],
+                    ["Energy", 10],
+                    ["Science", 10],
+                    ["Transportation", 10],
+                    ["Business", 10],
+                    ["Administration", 10]
+                ]);
+
+                var options = {
+                    title: "National Budget"
+                };
+
+                var chartElement = document.getElementById("piechart");
+                var chart = new google.visualization.PieChart(chartElement);
+                chart.draw(data, options);
+            }
+
+            function updateBudgetChart() {
+                var valueElements = document.getElementsByClassName("budgetValue");
+                var values = [];
+                for (var i = 0; i < valueElements.length; i++) values.push(parseInt(valueElements[i].value));
+
+                var data = google.visualization.arrayToDataTable([
+                    ["Department", "% Allocated"],
+                    ["Defense", values[0]],
+                    ["Healthcare", values[1]],
+                    ["Education", values[2]],
+                    ["Energy", values[3]],
+                    ["Science", values[4]],
+                    ["Transportation", values[5]],
+                    ["Business", values[6]],
+                    ["Administration", values[7]]
+                ]);
+
+                var options = {
+                    title: "National Budget"
+                };
+
+                var chartElement = document.getElementById("piechart");
+                var chart = new google.visualization.PieChart(chartElement);
+                chart.draw(data, options);
+            }
+
+            function updateEconFields() {
+                var econSysBox = document.getElementById("econSys");
+                var corpTaxBox = document.getElementById("corpTax");
+                if (econSysBox.value == "soc") {
+                    corpTaxBox.style.display = "none";
+                } else {
+                    corpTaxBox.style.display = "block";
+                }
+            }
+
+            function onSignIn(googleUser) {
+                var profile = googleUser.getBasicProfile();
+                var signinText = document.getElementById("g_signin_text");
+                signinText.innerHTML = "Signed-In as: " + profile.getName();
+
+                var proceedButton = document.getElementById("finalbutton");
+                proceedButton.style.display = "block";
+
+                var tokenBox = document.getElementById("tokenbox");
+                tokenBox.value = googleUser.getAuthResponse().id_token;
+            }
+        </script>
     </body>
 </html>
